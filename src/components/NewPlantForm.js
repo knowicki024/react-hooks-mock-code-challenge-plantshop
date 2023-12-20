70f8b6993f109923b6a2c5679fd4eae8dc0dfe87
Kaitlyn Nowicki <katie.nowicki024@gmail.com>
2023-12-20 12:34:35 -0500

    mock cc

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 
-function NewPlantForm() {
+function NewPlantForm({plants, setPlants, handleAddPlant}) {
+
+  const [newPlant, setNewPlant] = useState({
+    name: "",
+    image: "",
+    price: ""
+  });
+  function addNewPlant(newPlant) {
+    fetch("http://localhost:6001/plants", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(newPlant)
+    }
+  .then(res => res.json())
+  .then(setPlants))
+
+  }
+
+  function handleChange(e) {
+    setNewPlant({...newPlant, [e.target.name]: e.target.value});
+  }
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleChange}>
+        <input 
+        type="text" 
+        name="name" 
+        placeholder="Plant name" 
+        value={newPlant.name}
+        onChange={handleChange}
+        />
+        <input 
+        type="text" 
+        name="image" 
+        placeholder="Image URL" 
+        value={newPlant.image}
+        onChange={handleChange}
+        />
+        <input 
+        type="number"
+         name="price" 
+         step="0.01" 
+         placeholder="Price" 
+         value={newPlant.price}
+         onChange={handleChange}
+         />
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(res => res.json())
+    .then(setPlants)
+  }, [])
+
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
+
   return (
     <main>
-      <NewPlantForm />
+      <NewPlantForm handleAddPlant={handleAddPlant} />
       <Search />
-      <PlantList />
+      <PlantList plants={plants}/>
     </main>
   );
 }
