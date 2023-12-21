5de78f2c6c639c20b432a0580a59131b1a88a61e
Kaitlyn Nowicki <katie.nowicki024@gmail.com>
2023-12-21 10:46:27 -0500

    build and burn

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
+const API = "http://localhost:6001/plants"
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch(API)
+    .then(res => res.json())
+    .then(setPlants);
+  }, []);
+
+  const searchedPlants = plants.filter(eachPlant => {
+    return eachPlant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  })
+
+  const onAddNewPlant = (newlyAddedPlant) => {
+    setPlants([...plants, newlyAddedPlant])
+  }
+  const handleSearch =(e) => {
+    setSearchTerm(e.target.value)
+  }
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm 
+        onAddNewPlant={onAddNewPlant} 
+        API={API}
+      />
+      <Search 
+        onSearch={handleSearch}
+      />
+      <PlantList 
+        plants={searchedPlants}
+      />
     </main>
   );
 }
